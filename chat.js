333cda8046180895ffddbe3ee9a209e4ae9fda36
MaxenceMahieux <pro.maxencemahieux@gmail.com>
2023-11-23 14:51:16 +0100

    Correction du système d'heure

--- chat.js
+++ chat.js
@@ -56,16 +56,21 @@ io.on('connection', (socket) => {
     userCount++;
 
     let user = "user" + randomnumber(100);
-    let now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let time = hours + ':' + minutes;
+
+    // Envoi de l'heure toutes les 15 secondes à tous les clients connectés
+    setInterval(() => {
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let time = hours + ':' + (minutes < 10 ? '0' + minutes : minutes); // Ajoute un zéro pour les minutes < 10
+
+        io.emit('update_time', time); // Envoie la nouvelle heure à tous les clients connectés
+    }, 15000); // Rafraîchissement toutes les 15 secondes (15000 ms)
+
     socket.on('pseudo', (msg) => {
         user = msg;
         socket.emit('user_info', user);
-
     });
-
 
     io.emit('user_count', userCount);
     socket.emit('chat_clear');
@@ -78,12 +83,16 @@ io.on('connection', (socket) => {
 
         console.log('message de ' + user + ': ' + msg);
 
-        let message = '[' + time + '] ' + user + ': ' + msg;
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let time = hours + ':' + (minutes < 10 ? '0' + minutes : minutes); // Obtenir l'heure actuelle pour le message
+
+        let message = '[' + time + '] ' + user + ': ' + msg; // Utilisation de l'heure actuelle dans le message
 
         messages.push(message);
 
         io.emit('chat_emit', message);
-
     });
 
     socket.on('disconnect', () => {
